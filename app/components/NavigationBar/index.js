8f51fdb3f6becf4c799f18a89806bb6c1a522702
Halldór Valberg <halldor.valberg@hotmail.com>
2024-02-20 13:11:50 +0000

    Commented out unused navigation elements

--- app/components/NavigationBar/index.js
+++ app/components/NavigationBar/index.js
@@ -6,8 +6,8 @@ const NavigationBar = ({ activeElement, handleSelect }) => {
     <div className={styles.navigationBar}>
         <div className={styles.frame}>
             <a className={ activeElement === 'Home' ? styles.navigationElementActive : styles.navigationElement } onClick={() => handleSelect('Home')}>Home</a>
-            <a className={ activeElement === 'Projects' ? styles.navigationElementActive : styles.navigationElement } onClick={() => handleSelect('Projects')}>Projects</a>
-            <a className={ activeElement === 'Skills' ? styles.navigationElementActive : styles.navigationElement } onClick={() => handleSelect('Skills')}>Skills</a>
+            {/* <a className={ activeElement === 'Projects' ? styles.navigationElementActive : styles.navigationElement } onClick={() => handleSelect('Projects')}>Projects</a> */}
+            {/* <a className={ activeElement === 'Skills' ? styles.navigationElementActive : styles.navigationElement } onClick={() => handleSelect('Skills')}>Skills</a> */}
             <a className={ activeElement === 'Resume' ? styles.navigationElementActive : styles.navigationElement } onClick={() => handleSelect('Resume')}>Resume</a>
         </div>
     </div>
